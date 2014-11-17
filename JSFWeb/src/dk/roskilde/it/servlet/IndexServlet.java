8529deecaa8bfa3d681c748ba297cea355ce5ecb
heni123321 <henrik@haue.dk>
2014-11-17 10:16:05 +0100

    Addes login and index pages

---
+++ JSFWeb/src/dk/roskilde/it/servlet/IndexServlet.java
@@ -0,0 +1,39 @@
+package dk.roskilde.it.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class IndexServlet
+ */
+@WebServlet("/index")
+public class IndexServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public IndexServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		if (true) {
+			new LoginServlst().doGet(request, response);
+		} else {
+			RequestDispatcher requestDispatcher = request.getRequestDispatcher("/WEB-INF/pages/index.jsp");
+			requestDispatcher.forward(request, response);
+		}
+		
+	}
+
+}
---
+++ JSFWeb/src/dk/roskilde/it/servlet/LoginServlst.java
@@ -0,0 +1,23 @@
+package dk.roskilde.it.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginServlst extends HttpServlet {
+
+	private static final long serialVersionUID = -4587623677571227746L;
+	
+	public LoginServlst() {
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher requestDispatcher = request.getRequestDispatcher("/WEB-INF/pages/login.jsp");
+		requestDispatcher.forward(request, response);
+	}
+}
