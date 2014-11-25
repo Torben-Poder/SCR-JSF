c1deea6e2c6da99d63d90a3293d12060c2ab2edb
heni123321 <henrik@haue.dk>
2014-11-25 12:50:05 +0100

    add methods to  acces

--- JSFImpl/src/dk/roskilde/it/impl/Acces.java
+++ JSFImpl/src/dk/roskilde/it/impl/Acces.java
@@ -4,91 +4,97 @@ import java.util.List;
 
 import dk.roskilde.it.interfaces.AccesManager;
 import dk.roskilde.it.interfaces.Group;
+import dk.roskilde.it.interfaces.GroupManager;
+import dk.roskilde.it.interfaces.LoginManager;
+import dk.roskilde.it.interfaces.Membership;
 import dk.roskilde.it.interfaces.User;
 import dk.roskilde.it.interfaces.UserManager;
 
 public class Acces implements AccesManager {
 
-	static UserManager usermaneger;
+	UserManager usermaneger =  new MukUserManeger();
+	GroupManager groupmanager = new MukGroupManager();
+	Membership membership = new MukMembership();
+	LoginManager loginlanager = new MukLoginManager();
 
 	@Override
 	public List<Group> getGroups() {
-		return null;
+		return groupmanager.getGroups();
 	}
 
 	@Override
 	public boolean addGroup(Group group) {
-		return false;
+		return groupmanager.addGroup(group);
 	}
 
 	@Override
 	public boolean deleateGroup(String nme) {
-		return false;
+		return groupmanager.deleateGroup(nme);
 	}
 
 	@Override
 	public Group getGroup(String name) {
-		return null;
+		return groupmanager.getGroup(name);
 	}
 
 	@Override
 	public boolean login(User user) {
-		return false;
+		return loginlanager.login(user);
 	}
 
 	@Override
 	public boolean logout(User user) {
-		return false;
+		return loginlanager.logout(user);
 	}
 
 	@Override
 	public List<User> getUsersInGroup(Group group) {
-		return null;
+		return membership.getUsersInGroup(group);
 	}
 
 	@Override
 	public List<Group> getGroupsForUser(User user) {
-		return null;
+		return membership.getGroupsForUser(user);
 	}
 
 	@Override
 	public boolean addUserToGroup(User user, Group group) {
-		return false;
+		return membership.addUserToGroup(user, group);
 	}
 
 	@Override
 	public boolean remuveUserFromGroup(User user, Group group) {
-		return false;
+		return membership.remuveUserFromGroup(user, group);
 	}
 
 	@Override
 	public User getUser(String username) {
-		return null;
+		return usermaneger.getUser(username);
 	}
 
 	@Override
 	public User getUserByName(String name) {
-		return null;
+		return usermaneger.getUserByName(name);
 	}
 
 	@Override
 	public List<User> getAllUsers() {
-		return null;
+		return usermaneger.getAllUsers();
 	}
 
 	@Override
 	public boolean CreateUser(User user) {
-		return false;
+		return usermaneger.CreateUser(user);
 	}
 
 	@Override
 	public boolean UpdateUser(String username, User changedUser) {
-		return false;
+		return usermaneger.UpdateUser(username, changedUser);
 	}
 
 	@Override
 	public boolean DeleteUser(String username) {
-		return false;
+		return usermaneger.DeleteUser(username);
 	}
 
 }
